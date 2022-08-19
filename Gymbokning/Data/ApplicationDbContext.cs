f625f9ef5f4d608c340258cce4a40435842de8fb
Eknah <eknah@protonmail.com>
2022-08-19 10:00:08 +0200

    Add project files.

---
+++ Gymbokning/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Gymbokning.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gymbokning.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public DbSet<GymClass>? GymClass { get; set; }
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
---
+++ Gymbokning/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Gymbokning.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		// Navigation properties
+
+		public List<ApplicationUserGymClass> GymPasses { get; set; } = new();
+	}
+}
