0190bbe9f69075592795bec5c04cefd8f33ca2fb
Eknah <eknah@protonmail.com>
2022-08-22 09:36:06 +0200

    Add seed data for classes

--- Gymbokning/Data/SeedData.cs
+++ Gymbokning/Data/SeedData.cs
@@ -32,6 +32,59 @@ namespace Gymbokning.Data
 			};
 
 			await userManager.CreateAsync(normalUser, "Zxcvbn3#");
+
+			var gymClasses = AddGymClasses();
+
+			db.AddRange(gymClasses);
+
+			await db.SaveChangesAsync();
+		}
+
+		private static List<GymClass> AddGymClasses()
+		{
+			var gymClasses = new List<GymClass>();
+
+			gymClasses.Add(new GymClass()
+			{
+				Name = "Gympass A",
+				Description = "Ett gympass",
+				StartTime = DateTime.Now.AddDays(-2),
+				Duration = new TimeSpan(1, 10, 0)
+			});
+
+			gymClasses.Add(new GymClass()
+			{
+				Name = "Gympass B",
+				Description = "Ett till gympass",
+				StartTime = DateTime.Now.AddDays(-1),
+				Duration = new TimeSpan(1, 20, 0)
+			});
+
+			gymClasses.Add(new GymClass()
+			{
+				Name = "Gympass A",
+				Description = "Ännu ett gympass",
+				StartTime = DateTime.Now.AddDays(0),
+				Duration = new TimeSpan(1, 30, 0)
+			});
+
+			gymClasses.Add(new GymClass()
+			{
+				Name = "Gympass C",
+				Description = "Ytterligare ett gympass",
+				StartTime = DateTime.Now.AddDays(1),
+				Duration = new TimeSpan(0, 50, 0)
+			});
+
+			gymClasses.Add(new GymClass()
+			{
+				Name = "Gympass D",
+				Description = "Ett sista gympass",
+				StartTime = DateTime.Now.AddDays(2),
+				Duration = new TimeSpan(1, 10, 0)
+			});
+
+			return gymClasses;
 		}
 	}
 }
